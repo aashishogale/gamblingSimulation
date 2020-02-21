b4fde2cbcbcd6a13578048a01dfdc53af14d908f
aashishogale <aashishogale9@gmail.com>
2020-02-21 15:12:04 +0530

    added win or loss

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,2 +1,17 @@
-""
+#!/bin/bash -x
+
+echo "WELCOME TO GAMBLING SIMULATION"
+
+STAKE_PER_DAY=100
+BET=1
+
+#variables
+winOrLoss=$((RANDOM%2))
+
+if [ winOrLoss -eq 1]
+then
+	echo "win"
+else
+	echo "loss"
+fi
 
